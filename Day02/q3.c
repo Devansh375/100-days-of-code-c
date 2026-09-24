681dd05fa2ab2aa5e6d0b74d157d320e0356431e
Devansh375 <Devansh.40611@stu.upes.ac.in>
2026-09-24 19:29:58 +0000

    Day02/q4.c

--- Day02/q3.c
+++ Day02/q3.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 int main()
 {
-int a=4;
-int b=9;
-int area=4*9;
+int a=5;
+int b=7;
+int area=a*b;
 int perimeter=2*(a+b);
-printf("value of area=%d" ,area);
-printf("value of perimeter=%d", perimeter);
-printf("value of %d and %d",a ,b);
+printf("area of rectangle=%d" ,area);
+printf("perimeter of rectangle =%d", perimeter);
 return 0;
 }
---
+++ Day02/q4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int r=7;
+int area=(3.1416)*r*r;
+int circum=2*(3.1416*r);
+printf("area of circle=%d \n" ,area);
+printf("circumference =%d", circum);
+return 0;
+}
